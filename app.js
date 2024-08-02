fa8b9fa828cb9f9ebb6283eec9968e437fd018f1
Kavin07-kavi <Kavinkavi857@gmail.com>
2024-08-02 12:44:03 +0530

    hi

---
+++ app.js
@@ -0,0 +1,57 @@
+/*console.log('kavin')
+
+let a=10;
+let b=20;
+let c=a+b;
+let d=a-b;
+let e=a/b;
+let f=a*b;
+let g=a^b;
+let h=a%b;
+let i=1+a;
+let s='kavin';
+let t=10;
+let u=s+t;
+console.log(c,d,e,f,g,h,i,u);
+console.log(typeof(c))*/
+
+/*let a=100;
+let b=200;
+if(a>b)
+    {
+     console.log(a);
+    }
+ else
+    {
+     console.log(b);
+    }*/
+
+ 
+/*let a=10;
+
+if(a % 2 == 0) {
+    console.log("The number is even.");
+}
+else {
+    console.log("The number is odd.");
+
+}*/
+
+/*let age = 10;
+
+if (age >= 18  ) 
+ {
+  console.log("youngester");
+ } 
+else
+ {
+  console.log("children");
+ }*/
+
+  let i=0;
+  do{
+   console.log(i)
+   i++;
+  }
+  while(i<=10);
+
